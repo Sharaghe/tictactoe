74dc89c195c0b52a7ac1898265044ed58367fe6d
Florian Kattner <florian.kattner@gmail.com>
2022-08-12 21:31:07 +0200

    Clicking the tiles now alternatingly changes the symbols (x and o)

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+const Player = (name, symbol, color) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const getColor = () => color;
+
+    return {getName, getSymbol, getColor};
+}
+
 const gameBoard = (() => {
 
     const tiles = document.querySelectorAll(".tile");
@@ -11,30 +20,70 @@ const gameBoard = (() => {
         index++;
     });
 
+    const tileClicked = (e) =>{
+        let clickedID = e.target.getAttribute("data-id");
+        targetTile = gameBoard.getSingleTile(clickedID);
+        markTile(targetTile);
+        gameController.turnFinished();
+    }
+
+    const markTile = (targetTile) =>{
+        if(!targetTile.isOccupied){
+            targetTile.node.textContent = gameController.getCurrentPlayer().getSymbol();
+        }
+    }
+    
+    const isEmpty = (tileContent) =>{
+        return tileContent != "x" && tileContent != "o";
+    }
+
     const getSingleTile = (id) => {
         return allTiles.find((element) => element.id == id);
     }
 
-    return { setupTiles, getSingleTile };
+    return { setupTiles, getSingleTile, isEmpty };
 
 })();
 
-gameBoard.setupTiles();
+const gameController = (() => {
+
+    const ROUNDSTOPLAY = 9;
+    const player1 = Player("Player 1", "x", "blue");
+    const player2 = Player("Player 2", "o", "red");
+
+    let currentPlayer = player1;
+
+    const turnFinished = (id, symbol) => {
+        setCurrentPlayer((getCurrentPlayer() == player1) ? player2 : player1);
+    }
+
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    }
+
+    const setCurrentPlayer = (player) => {
+        currentPlayer = player;
+    }
+
+    const checkForWinner = () => {
+
+    }
+
+    return { turnFinished, getCurrentPlayer, checkForWinner };
+})();
 
 function Tile(node, id){
     this.node = node;
     this.id = id;
     this.isOccupied = false;
-    this.setOccupied = (value) => {
+    const setOccupied = (value) => {
         this.isOccupied = value;
     }
-    this.getOccupied = () => {
+    const isOccupied = () => {
         return this.isOccupied;
     }
+
+
 }
 
-function tileClicked(e){
-    let clickedID = e.target.getAttribute("data-id");
-    targetTile = gameBoard.getSingleTile(clickedID);
-    console.log(targetTile);
-}
+gameBoard.setupTiles();
